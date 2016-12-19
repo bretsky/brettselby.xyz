c4856963e8a5dab8fb6b6141f14a05f96f4f24c7
bretsky <bretskyselby99@gmail.com>
2016-12-19 03:27:59 -0800

    Change quote generator name.

---
+++ cgi-bin/quote-gen.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import cgi, cgitb
+import json
+import random
+
+cgitb.enable()
+
+print("Content-Type: text/html;charset=utf-8\n")
+
+def choose_quote():
+	quotes = json.load(open("quotes.json", "r"))["quotes"]
+	return random.choice(quotes)
+
+def generate_hmtl(quote):
+		return """<span>&quot;{}&quot;</span><br><span class="quoteauthor">-{}</span>""".format(quote[0], quote[1])
+
+print(generate_hmtl(choose_quote()))
