5d2c0fa0d6c3364171517536a3b0f2e509d64ed5
bretsky <bretskyselby99@gmail.com>
2018-01-20 06:04:31 -0500

    Update website for 1B

--- js/quotes.js
+++ js/quotes.js
@@ -1,7 +1,27 @@
 function replaceQuote() {
 	$.get("cgi-bin/quote-gen.py", function(data) {
 		document.getElementById("quote").innerHTML = data
+		var quote = $("#quotetext");
+	    var quoteAuthor = $(".quoteauthor");
+	    var quoteCell = $("#quote");
+	    quoteCell.height(quote.height() + quoteAuthor.height());
 	})
 }
 
+
+
+
+function resizeBackground() {
+	var bg = $("html");
+	bg.height(window.innerHeight);
+    var quote = $("#quotetext");
+    var quoteAuthor = $(".quoteauthor");
+    var quoteCell = $("#quote");
+    quoteCell.height(quote.height() + quoteAuthor.height());
+}
+
 window.onload = replaceQuote()
+$(window).resize(resizeBackground);
+var bg = $("html");
+bg.height(window.innerHeight);
+resizeBackground()
